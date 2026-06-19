0935e847ae2c8b9cf26ae18d5456c504b62a637d
piyushbasist <vasisthpiyush@gmail.com>
2026-06-20 01:15:29 +0530

    Add GetForUser route test

--- CommBank.Tests/GoalControllerTests.cs
+++ CommBank.Tests/GoalControllerTests.cs
@@ -63,12 +63,27 @@ public class GoalControllerTests
     }
 
     [Fact]
-    public async void GetForUser()
+    [Fact]
+public async void GetForUser()
+{
+    // Arrange
+    var goals = collections.GetGoals();
+    var users = collections.GetUsers();
+    IGoalsService goalsService = new FakeGoalsService(goals, goals[0]);
+    IUsersService usersService = new FakeUsersService(users, users[0]);
+    GoalController controller = new(goalsService, usersService);
+
+    // Act
+    var httpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext();
+    controller.ControllerContext.HttpContext = httpContext;
+    var result = await controller.GetForUser(users[0].Id!);
+
+    // Assert
+    Assert.NotNull(result);
+
+    foreach (Goal goal in result!)
     {
-        // Arrange
-        
-        // Act
-        
-        // Assert
+        Assert.IsAssignableFrom<Goal>(goal);
+        Assert.Equal(users[0].Id, goal.UserId);
     }
 }
